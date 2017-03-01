376b79645ff9f8381211dd09e6c0476d0411ed6b
root <root@localhost.localdomain>
2017-03-01 18:56:48 +0530

    connect to mongodb with mongojs

--- mytasklist/routes/tasks.js
+++ mytasklist/routes/tasks.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://oliver7:75Hz4815@@ds113000.mlab.com:13000/myapp_task_manager',['tasks'])
 
 router.get('/tasks', function(req, res) {
 
-	res.send('API PAGE');
+	db.tasks.find(function(err, tasks){
+
+		if (err) {
+			res.send(err);
+		} 
+			
+			else{
+				res.json(tasks);
+			};
+	})
 
 
 });
